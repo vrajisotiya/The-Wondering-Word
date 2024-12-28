403a0104b4a84fe96fe477a35e30cb51758c45e4
vrajisotiya <vrajisotiya88@gmail.com>
2024-12-28 11:53:12 +0530

    f

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
-import conf from "../conf/conf";
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
@@ -13,7 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey={conf.tinyMce}
+            apiKey={import.meta.env.VITE_TINYMCE}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
